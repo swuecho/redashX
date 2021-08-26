b1bd9755d11ae22d624268035f12863760370e6f
Hao Wu <echowuhao@gmail.com>
2021-08-27 01:20:07 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import RedashPlot from './PlotRedashData.js'
+import RedashTable from './RedashTable.js'
 import Redash from './Redash.js'
 import "antd/dist/antd.css";
 
@@ -11,6 +12,7 @@ function App() {
   return (
     <div className="App">
       <Content></Content>
+      <RedashTable></RedashTable>
       <RedashPlot/>
       <Redash/>
     </div>
---
+++ src/RedashTable.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { useRedashQueryData } from "./redashData";
+import ReactJson from 'react-json-view'
+
+// query, id.
+// plot configuration
+
+function RedashTable() {
+        const proData = useRedashQueryData(65);
+        return (
+                <div>
+                        <ReactJson src={proData}></ReactJson>
+                </div>
+
+        );
+}
+
+export default RedashTable;
