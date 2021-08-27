ef7b2eb107c98184319fb0f78708e3d5312e6133
Hao Wu <echowuhao@gmail.com>
2021-08-27 12:34:20 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.css';
-import RedashPlot from './PlotRedashData.js'
 import "antd/dist/antd.css";
 
 
 /* eslint-disable import/no-webpack-loader-syntax */
 import Content from '!babel-loader!@mdx-js/loader!./Content.mdx'
 
+
 function App() {
   return (
     <div className="App">
       <Content></Content>
-      <RedashPlot/>
+   
     </div>
   );
 }
