bc1cb367d69591281ec099a14457ee3f5d04919a
Hao Wu <wuhaoecho@gmail.com>
2021-08-31 16:31:49 +0800

    update (#20)

--- src/pages/admin/sub-page2.tsx
+++ src/pages/admin/sub-page2.tsx
@@ -1,9 +1,60 @@
 import React, { useState } from 'react';
+import { Button, Descriptions, Result,  Space, Statistic } from 'antd';
+import { LikeOutlined } from '@ant-design/icons';
 import { PageContainer } from '@ant-design/pro-layout';
-
-export default () => {
+import ProEditTable from "./../../components/demo/ProEditTable";
+const content = (
+    <Descriptions size="small" column={2}>
+      <Descriptions.Item label="创建人">张三</Descriptions.Item>
+      <Descriptions.Item label="联系方式">
+        <a>421421</a>
+      </Descriptions.Item>
+      <Descriptions.Item label="创建时间">2017-01-10</Descriptions.Item>
+      <Descriptions.Item label="更新时间">2017-10-10</Descriptions.Item>
+      <Descriptions.Item label="备注">中国浙江省杭州市西湖区古翠路</Descriptions.Item>
+    </Descriptions>
+  );
+function PageContainerX() {
     return <PageContainer
+        content={content}
+        tabList={[
+            {
+                tab: '基本信息',
+                key: 'base',
+            },
+            {
+                tab: '详细信息',
+                key: 'info',
+            },
+        ]}
+        extraContent={
+            <Space size={24}>
+                <Statistic title="Feedback" value={1128} prefix={<LikeOutlined />} />
+                <Statistic title="Unmerged" value={93} suffix="/ 100" />
+            </Space>
+        }
+        extra={[
+            <Button key="3">操作</Button>,
+            <Button key="2">操作</Button>,
+            <Button key="1" type="primary">
+                主操作
+            </Button>,
+        ]}
+        footer={[
+            <Button key="3">重置</Button>,
+            <Button key="2" type="primary">
+                提交
+            </Button>,
+        ]}
     >
-        <div>New Page admin 2 </div>
+        <div
+            style={{
+                height: '120vh',
+            }}
+        >
+            <ProEditTable/>
+        </div>
     </PageContainer>
-};
+}
+
+export default PageContainerX;
