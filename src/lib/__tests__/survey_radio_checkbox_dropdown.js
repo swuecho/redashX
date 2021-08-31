7fdc6519b36b1ab4f1ba003a9052629a09c52bdd
Hao Wu <echowuhao@gmail.com>
2021-09-01 00:30:00 +0800

    update

---
+++ src/lib/__tests__/survey_radio_checkbox_dropdown.js
@@ -0,0 +1,47 @@
+import { surveyjsQuestion2ProTable } from '../survey'
+import {
+    survey2ProTable
+} from '../survey.js'
+
+// each page, each form
+// TODO: only do first page.
+
+let survey_json = {
+    "pages": [{
+        "name": "page1", "elements":
+            [{
+                "title": "状态", "choices":
+                    [{ "value": "1", "text": "item 1", "meta": {} },
+                    { "value": "2", "text": "item 2", "meta": {} },
+                    { "value": "3", "text": "item 3", "meta": {} }], "name": "Q1", "type": "dropdown"
+            }]
+    }]
+}
+
+let input_json = survey_json['pages'][0]["elements"][0]
+
+let expected_json = {
+    "title": "状态",
+    "dataIndex": "Q1",
+    "valueType": "select",
+    "fieldProps": {
+        "options": [
+            {
+                "label": "item 1",
+                "value": "1"
+            },
+            {
+                "label": "item 2",
+                "value": "2"
+            },
+            {
+                "label": "item 3",
+                "value": "3"
+            }
+        ]
+    }
+}
+
+test('rand str', () => {
+    expect(surveyjsQuestion2ProTable(input_json)).toEqual(expected_json)
+})
---
+++ src/lib/survey.js
@@ -0,0 +1,40 @@
+import { transform } from "node-json-transform";
+
+
+const type_mapper = {
+    radiogroup: "radio",
+    checkbox: "checkbox",
+    dropdown: "select",
+    text: "text",
+    comment: "textarea"
+}
+
+export function surveyjsQuestion2ProTable(surveyjsQuestion) {
+
+    let result = transform(surveyjsQuestion, {
+        item: {
+            title: "title",
+            dataIndex: "name",
+            fieldProps: { options: "choices" },
+            valueType: "type"
+        },
+        operate: [
+            {
+                run: function (val) { return type_mapper[val] }, on: "valueType"
+            },
+            {
+                run: function (opts) { return opts ? opts.map(opt => ({ label: opt.text, value: opt.value })) : undefined }, on: "fieldProps.options"
+            }
+        ],
+        // Not required.  Runs after object mapping and operations.  Allows access to each item for manipulation.
+        each: function (item) {  
+            let textType = new Set(["text", "textarea"])
+            if (textType.has(item['valueType'])) {
+                delete item['fieldProps']
+            }
+            return item;
+        }
+    }
+    )
+    return result
+}
