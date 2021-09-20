f3232e936c6476a5345b14733a3f60a386b30d81
Hao Wu <echowuhao@gmail.com>
2021-09-20 11:27:13 +0800

    update

--- src/pages/report/StockCb.tsx
+++ src/pages/report/StockCb.tsx
@@ -6,15 +6,16 @@ import StarRating from "./../../components/learn/starRating.js";
 function CbWatch() {
     return (
         <div className="bg-white">
-            <div className="text-center">
-                <StarRating stars={5} />
-                <CbSummary />
+            <div className="p-10">
+                <div className="text-center">
+                    <h1> 可转债观察 </h1>
+                    <StarRating stars={5} />
+                    <CbSummary />
+                </div>
+                <RedashPlot queryId={53} plotId={76}></RedashPlot>
+
+                <RedashPlot queryId={31} plotId={46}></RedashPlot>
             </div>
-            <RedashPlot queryId={53} plotId={76}></RedashPlot>
-
-            <RedashPlot queryId={31} plotId={46}></RedashPlot>
-
-            <hr />
         </div >
     )
 
