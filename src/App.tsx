530777caae51795498325d8e92d42b889b64895a
Hao Wu <wuhaoecho@gmail.com>
2022-06-26 12:04:54 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -5,30 +5,9 @@ import {
     useLocation,
 } from "react-router-dom";
 
-import MyProTable from "./components/demo/ProEditTable";
-import Survey from "./bestqa/Survey";
 
-import ExcelView from "./bestqa/Excel";
 
 import ProHome from './ProHome';
-
-// A custom hook that builds on useLocation to parse
-// the query string for you.
-function useQuery() {
-    return new URLSearchParams(useLocation().search);
-}
-
-
-
-function ViewExecl() {
-    let query = useQuery();
-    let name = query.get('name')
-    let eid = query.get('eid')
-    if (name && eid) {
-        return <ExcelView name={name} eid={eid as unknown as number}></ExcelView>
-    }
-    return null;
-}
 
 const App = () => {
 
@@ -38,15 +17,6 @@ const App = () => {
                 {/* A <Switch> looks through its children <Route>s and
         renders the first one that matches the current URL. */}
                 <Switch>
-                    <Route path="/users">
-                        <MyProTable />
-                    </Route>
-                    <Route path="/excel">
-                        <ViewExecl></ViewExecl>
-                    </Route>
-                    <Route path="/table">
-                        <MyProTable />
-                    </Route>
                     <Route path="/">
                         <ProHome />
                     </Route>
